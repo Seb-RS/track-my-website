0f951d5047960ea1115b6c3fc8bb312d9e0b7e08
Seb-RS <sebadelhns@gmail.com>
2024-03-19 20:51:37 -0300

    created StatusCard partial component

---
+++ resources/js/Pages/Home/Partials/StatusCard.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="w-full bg-neutral-700 rounded-lg p-3 flex-col">
+        <h1 class="text-neutral-50">Sebdev Portfolio</h1>
+        <div class="flex overflow-x-auto py-1">
+            <div class="flex min-w-[528px] justify-between">
+                <div class="w-[6.8px] h-[35px] bg-transparent/15 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+                <div class="w-[6.8px] h-[35px] bg-green-300 rounded-lg"></div>
+            </div>
+        </div>
+        <div class="text-neutral-300 flex justify-between italic text-xs">
+            <h2>60 días</h2>
+            <h2>Actualidad</h2>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+::-webkit-scrollbar {
+    width: 3px;
+    height: 3px;
+}
+
+::-webkit-scrollbar-thumb {
+    background-color: #848484;
+    border-radius: 5px;
+}
+</style>
