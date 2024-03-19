9d3ce45741fdb48d88373af85724358b9ecc6c2e
Seb-RS <sebadelhns@gmail.com>
2024-03-19 20:52:50 -0300

    style: imported StatusCard partial component in Home/Show view

--- resources/js/Pages/Home/Show.vue
+++ resources/js/Pages/Home/Show.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        Your Web Tracker
+    <div class="min-h-screen max-h-max bg-gradient-to-t bg-neutral-900">
+        <div class="mx-auto w-full max-w-screen-md px-4 pb-8 pt-20 flex justify-center">
+            <div class="max-w-xl w-full h-full flex flex-col justify-center space-y-10">
+                <div class="w-full bg-neutral-800 rounded-lg p-3 flex-col space-y-2">
+                    <StatusCard />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+import StatusCard from './Partials/StatusCard.vue';
+
+export default {
+    components: {
+        StatusCard
+    }
+}
+</script>
