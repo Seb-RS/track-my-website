90128bca0e632e11e974a9711cf6db59be3df6ac
Seb-RS <sebadelhns@gmail.com>
2024-03-19 20:54:06 -0300

    feat: created Schedule call function to ping and register result

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Models\PingResult;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    $urls = ['https://sebdev.cl', 'https://lab.sebdev.cl'];
+
+    foreach ($urls as $url) {
+        $response = Http::get($url);
+        $statusCode = $response->status();
+
+        PingResult::create([
+            'url' => $url,
+            'status_code' => $statusCode
+        ]);
+    }
+})->everyFiveMinutes();
