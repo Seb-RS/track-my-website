9af5b1ef001d53e022002d2d69600a9ba22fda44
Seb-RS <sebadelhns@gmail.com>
2024-03-19 21:37:05 -0300

    add: OkCircleFilled as an Icon Component

---
+++ resources/js/Components/Icons/OkCircleFilled.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" xml:space="preserve" viewBox="0 0 24 24"
+        fill="currentColor">
+        <path
+            d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm-1.2 14.8-3.7-3.7 1.4-1.4 2.2 2.2 5.8-6.1L18 9.3l-7.2 7.5z" />
+        <path d="M0 0h24v24H0z" fill="none" />
+    </svg>
+</template>
