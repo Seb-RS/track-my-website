8e83e7005ce2c17d7924bea177e06b095ca0412a
Seb-RS <sebadelhns@gmail.com>
2024-03-19 21:38:12 -0300

    feat: imported OkCircleFilled Icon Component next to the text 'Disponible'

--- resources/js/Pages/Home/Partials/StatusCard.vue
+++ resources/js/Pages/Home/Partials/StatusCard.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="w-full bg-neutral-700 rounded-lg p-3 flex-col">
-        <h1 class="text-neutral-50">Sebdev Portfolio</h1>
+        <div class="flex justify-between text-sm">
+            <h1 class="text-neutral-50">Sebdev Portfolio</h1>
+            <h1 class="text-neutral-50 flex items-center">
+                Disponible
+                <span class="ml-1 text-green-200">
+                    <OkCircleFilled />
+                </span>
+            </h1>
+        </div>
         <div class="flex overflow-x-auto py-1">
             <div class="flex min-w-[528px] justify-between">
                 <div class="w-[6.8px] h-[35px] bg-transparent/15 rounded-lg"></div>
@@ -71,6 +79,17 @@
         </div>
     </div>
 </template>
+
+<script>
+import OkCircleFilled from '@/Components/Icons/OkCircleFilled.vue';
+
+export default {
+    components: {
+        OkCircleFilled
+    }
+}
+
+</script>
 
 <style scoped>
 ::-webkit-scrollbar {
